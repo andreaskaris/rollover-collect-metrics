476827bf60607bba131e5b815727136d6cb88ecb
Andreas Karis <ak.karis@gmail.com>
2023-07-07 04:24:40 +0200

    first
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

---
+++ gather.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+ACTION="$1"
+
+GATHER_DIR=/var/gather-data
+IDENTIFIER=0
+ARCHIVE=/tmp/archive.tar.gz
+
+help() {
+  local prog
+  prog=$(basename "$0")
+  echo "./$prog"
+  echo "    help"
+  echo "          print this help text"
+  echo "    start [gather directory] [identifier]"
+  echo "          start data gathering in directory ${GATHER_DIR}"
+  echo "    stop [identifier]"
+  echo "          stop data gathering"
+  echo "    cleanup [gather directory] [identifier]"
+  echo "          stop data gathering and delete data gathering directory"
+  echo "    archive [gather directory] [identifier] [archive])"
+  echo "          stop data gathering, tar up contents to ${ARCHIVE} and delete gathering directory"
+  echo "Example: ./$prog start /var/gather-data2"
+}
+
+gather_dir_exists() {
+  if [ -d "${GATHER_DIR}" ]; then
+    echo "Directory ${GATHER_DIR} exists. Delete it first (run cleanup action)"
+    exit 1
+  fi
+}
+
+gather_dir_does_not_exist() {
+  if ! [ -d "${GATHER_DIR}" ]; then
+    echo "Directory ${GATHER_DIR} does not exist. Nothing to do"
+    exit 1
+  fi
+}
+
+gather() {
+  echo "Gathering data in ${GATHER_DIR} with identifier ${IDENTIFIER}"
+  mkdir -p "${GATHER_DIR}"
+
+  podman run \
+    --name "gather-monitor-${IDENTIFIER}" \
+    --privileged \
+    --network=host \
+    -v /proc/:/proc \
+    -v "${GATHER_DIR}:/gather-dir:z" \
+    -d --rm \
+    quay.io/akaris/must-gather-network-metrics:v0.4 \
+    /bin/bash -c "mkdir /gather-dir/monitor; cd /gather-dir/monitor; export SS_OPTS=\"-noemitaupwS\"; bash /resources/monitor.sh -d 10"
+  podman run \
+    --name "gather-pidstat-${IDENTIFIER}" \
+    -v "${GATHER_DIR}:/gather-dir:z" \
+    -d --rm \
+    --privileged \
+    -v /proc/:/proc \
+    --pid=host \
+    quay.io/akaris/must-gather-network-metrics:v0.4 \
+    /bin/bash -c "pidstat -p ALL -T ALL -I -l -r -t -u -d -w 5 > /gather-dir/pidstat.txt"
+}
+
+stop_gather() {
+  containers="gather-monitor-${IDENTIFIER} gather-pidstat-${IDENTIFIER}"
+
+  echo "Stopping gather with identifier ${IDENTIFIER}"
+  for c in ${containers}; do
+    if podman ps | grep -q "$c"; then
+      echo "Stopping container ${c}"
+      podman stop "${c}"
+    fi
+  done
+
+  sleep 5
+
+  echo "Force removing left over containers for identifier ${IDENTIFIER}"
+  for c in ${containers}; do
+    if podman ps -a | grep -q "$c"; then
+      echo "Force removing container ${c}"
+      podman rm -f "${c}"
+    fi
+  done
+}
+
+cleanup() {
+  echo "Deleting ${GATHER_DIR}"
+  rm -Rf "${GATHER_DIR}" 2>/dev/null
+}
+
+archive_exists() {
+  if [ -f "${ARCHIVE}" ]; then
+    echo "${ARCHIVE} exists. Delete it first"
+    exit 1
+  fi
+}
+
+archive() {
+  echo "Archiving ${GATHER_DIR} to ${ARCHIVE}"
+  ionice -c2 -n7 nice -n19 tar -czf "${ARCHIVE}" "${GATHER_DIR}"
+}
+
+if [ "${ACTION}" == "start" ]; then
+  GATHER_DIR=${2:-$GATHER_DIR}
+  IDENTIFIER=${3:-$IDENTIFIER}
+  gather_dir_exists
+  gather
+elif [ "${ACTION}" == "stop" ]; then
+  IDENTIFIER=${2:-$IDENTIFIER}
+  stop_gather
+elif [ "${ACTION}" == "cleanup" ]; then
+  GATHER_DIR=${2:-$GATHER_DIR}
+  IDENTIFIER=${3:-$IDENTIFIER}
+  gather_dir_does_not_exist
+  stop_gather
+  cleanup
+elif [ "${ACTION}" == "archive" ]; then
+  GATHER_DIR=${2:-$GATHER_DIR}
+  IDENTIFIER=${3:-$IDENTIFIER}
+  ARCHIVE=${4:-$ARCHIVE}
+  archive_exists
+  gather_dir_does_not_exist
+  stop_gather
+  archive
+  cleanup
+else
+  help
+fi
---
+++ rollover.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+DATA_DIR=/var/debug-data
+MAX_ITERATIONS=5
+TIME_BETWEEN_ROLLOVERS=10
+
+function ctrl_c() {
+  echo "CTRL-C - cleaning up"
+  i=0
+  while [ $i -lt $MAX_ITERATIONS ]; do 
+    "${DIR}"/gather.sh stop "${i}"
+    i=$((i+1))
+  done
+  exit 0
+}
+
+trap ctrl_c SIGINT
+
+mkdir "${DATA_DIR}"
+
+# Run first iteration.
+iteration=0
+echo "On iteration ${iteration}"
+rm -Rf "${DATA_DIR:?}/${iteration:?}"
+"${DIR}"/gather.sh start "${DATA_DIR}/${iteration}" "${iteration}"
+
+# Now start with second iteration.
+while true; do 
+  sleep $TIME_BETWEEN_ROLLOVERS
+
+  old_iteration=$iteration
+  iteration=$((iteration+1))
+  iteration=$((iteration%MAX_ITERATIONS))
+
+  echo "On iteration ${iteration}"
+  rm -Rf "${DATA_DIR:?}/${iteration:?}"
+  "${DIR}"/gather.sh start "${DATA_DIR}/${iteration}" "${iteration}"
+
+  echo "Archiving iteration ${old_iteration}"
+  rm -f "${DATA_DIR}/${old_iteration}.tar.gz"
+  "${DIR}"/gather.sh archive "${DATA_DIR}/${old_iteration}" "${old_iteration}" "${DATA_DIR}/${old_iteration}.tar.gz"
+done
